022375c86b232a02b5ce3e2745f68eabdd53a42c
unknown <you@example.com>
2019-06-08 00:58:19 +0600

    Add View, Vote and Answer

--- app/Question.php
+++ app/Question.php
@@ -19,7 +19,13 @@ class Question extends Model
     public function getUrlAttribute(){
         return route("questions.show",$this->id);
     }
-    public function getCreatedDateAttribute(){
-        return $this->created_at->diffForHumans();
+    public function getStatusAttribute(){
+        if($this->answers > 0){
+            if($this->best_answer_id){
+                return "answerd-accepted";
+            }
+            return "answered";
+        }
+        return "unanswered";
     }
 }
